2c7b7b10ca43575eab00a78610afdec42bc0b62c
clearsky77 <clearsky3264@gmail.com>
2021-10-05 21:30:14 +0900

    화면 전환 구현 (메인 페이지 -> 캐터 리스트 페이지) intent

--- app/src/main/java/com/clearsky77/mycharacterlist_20211005/MainActivity.kt
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.clearsky77.mycharacterlist_20211005
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toMyCharListBtn.setOnClickListener {
+            val myIntent = Intent(this, CharListActivity :: class.java)
+            startActivity(myIntent)
+        }
     }
 }
