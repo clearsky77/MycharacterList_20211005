a66e6d1e7bc13285caa6c8872afe7813d23e0f4d
clearsky77 <clearsky3264@gmail.com>
2021-10-05 21:18:12 +0900

    메인 화면 작성

---
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
@@ -0,0 +1,11 @@
+package com.clearsky77.mycharacterlist_20211005
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_char_list)
+    }
+}
