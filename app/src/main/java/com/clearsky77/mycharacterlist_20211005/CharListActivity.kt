ace2047286561d75c74bfe39b4507b84efdebe2a
clearsky77 <clearsky3264@gmail.com>
2021-10-05 22:33:09 +0900

    뷰페이저 어댑터 생성. 어댑터와 Activity를 연결하여 UI에 표현.

--- app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
@@ -2,10 +2,17 @@ package com.clearsky77.mycharacterlist_20211005
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.mycharacterlist_20211005.atapters.ImageViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_char_list.*
 
 class CharListActivity : AppCompatActivity() {
+    lateinit var mvpa : ImageViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_char_list)
+
+        mvpa = ImageViewPagerAdapter(supportFragmentManager) // 어댑터를 만들고
+        charListViewPager.adapter = mvpa // xml에 어댑터를 얻는다
     }
 }
