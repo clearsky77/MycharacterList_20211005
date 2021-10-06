eb77737e0ead940da3a21f3c403b9df39bbe0a15
clearsky77 <clearsky3264@gmail.com>
2021-10-06 23:13:49 +0900

    ViewPageAdapter 총 3개의 프래그먼트가 조건에 맞게 보이도록

--- app/src/main/java/com/clearsky77/mycharacterlist_20211005/atapters/ImageViewPagerAdapter.kt
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/atapters/ImageViewPagerAdapter.kt
@@ -4,14 +4,23 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.clearsky77.mycharacterlist_20211005.fragments.Posts01Fragment
+import com.clearsky77.mycharacterlist_20211005.fragments.Posts02Fragment
+import com.clearsky77.mycharacterlist_20211005.fragments.Posts03Fragment
 
-class ImageViewPagerAdapter(fm : FragmentManager) :FragmentPagerAdapter(fm) {
-//    몇 장의 뷰를 만들 것인가?
+class ImageViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    //    몇 장의 뷰를 만들 것인가?
     override fun getCount(): Int {
-        return 1 //테스트로 하나의 프래그먼트를 올려보자
+        return 3 //테스트로 하나의 프래그먼트를 올려보자
     }
-//  번호에 따른 프래그먼트 반환
+
+    //  번호에 따른 프래그먼트 반환
     override fun getItem(position: Int): Fragment {
+        if(position == 0) {
             return Posts01Fragment() //테스트로 하나의 프래그먼트를 올려보자
+        }else if(position == 1){
+            return Posts02Fragment()
+        }else{
+            return Posts03Fragment()
+        }
     }
 }
