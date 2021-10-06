b2dc716ede42837e1d1a8782a9a0310333abb97f
clearsky77 <clearsky3264@gmail.com>
2021-10-06 22:52:27 +0900

    프레그먼트 3개 중 3개 완료 - kt, xml 2개씩 추가 해줌

---
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/fragments/Posts03Fragment.kt
@@ -0,0 +1,35 @@
+package com.clearsky77.mycharacterlist_20211005.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.clearsky77.mycharacterlist_20211005.R
+import kotlinx.android.synthetic.main.fragment_posts01.*
+
+class Posts03Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_posts01, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        postImg01.setOnClickListener {
+//            Toast.makeText(requireContext(), "postImg01.setOnClickListener", Toast.LENGTH_SHORT).show()
+//        }
+        val imgeURL = "https://ssl.nx.com/s2/game/mabinogi/MabiWeb/event/2020/1119_memory_m5d2rk/listb_201119_memory.jpg"
+        Glide.with(this).load(imgeURL).into(postImg01)
+        val imgeURL2 = "https://ssl.nx.com/s2/game/mabinogi/MabiWeb/event/2021/0909_bambul_29wse3/listb_210909_light.jpg"
+        Glide.with(this).load(imgeURL2).into(postImg02)
+        val imgeURL3 = "https://ssl.nx.com/s2/game/mabinogi/MabiWeb/event/2021/0805_teaparty_fjdn2z/listb_210805_teaparty.jpg"
+        Glide.with(this).load(imgeURL3).into(postImg03)
+
+    }
+}
