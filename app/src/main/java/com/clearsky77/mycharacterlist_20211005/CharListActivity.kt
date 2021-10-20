72919d8fc3171dc9ee3a0baf2c7a9955207e424b
clearsky77 <clearsky3264@gmail.com>
2021-10-21 02:05:56 +0900

    탭(PageTitle)에 이미지 얹음  - padding 설정  - setCustomView 활용  - 기존 이미지 교체

--- app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
@@ -2,6 +2,7 @@ package com.clearsky77.mycharacterlist_20211005
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.clearsky77.mycharacterlist_20211005.atapters.ImageViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_char_list.*
 
@@ -19,9 +20,19 @@ class CharListActivity : AppCompatActivity() {
         charListTabLayout.setupWithViewPager(charListViewPager)
 
         // 탭에 이미지 얹기
-        charListTabLayout.getTabAt(0)?.setIcon(R.drawable.character01)
-        charListTabLayout.getTabAt(1)?.setIcon(R.drawable.character02)
-        charListTabLayout.getTabAt(2)?.setIcon(R.drawable.character03)
+        val charImg01 = ImageView(this)
+        charImg01.setImageResource(R.drawable.character01)
+        charImg01.setPadding(35,35,35,35)
+        val charImg02 = ImageView(this)
+        charImg02.setImageResource(R.drawable.character02)
+        charImg02.setPadding(35,35,35,35)
+        val charImg03 = ImageView(this)
+        charImg03.setImageResource(R.drawable.character03)
+        charImg03.setPadding(35,35,35,35)
+
+        charListTabLayout.getTabAt(0)?.setCustomView(charImg01)
+        charListTabLayout.getTabAt(1)?.setCustomView(charImg02)
+        charListTabLayout.getTabAt(2)?.setCustomView(charImg03)
 
     }
 }
