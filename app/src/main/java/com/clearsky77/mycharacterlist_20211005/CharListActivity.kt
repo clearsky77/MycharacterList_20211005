b09a737e1ad8435fdbded76c803d7ea48082084a
clearsky77 <clearsky3264@gmail.com>
2021-10-24 19:58:27 +0900

    네비게이션뷰 적용하기 4. 햄버거 아이콘 터치 시 동작 / 5.네비게이션 메뉴 터치 시 동작 틀 작성

--- app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
+++ app/src/main/java/com/clearsky77/mycharacterlist_20211005/CharListActivity.kt
@@ -2,11 +2,15 @@ package com.clearsky77.mycharacterlist_20211005
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.clearsky77.mycharacterlist_20211005.atapters.ImageViewPagerAdapter
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_char_list.*
 
-class CharListActivity : AppCompatActivity() {
+class CharListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var mvpa : ImageViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,5 +38,23 @@ class CharListActivity : AppCompatActivity() {
         charListTabLayout.getTabAt(1)?.setCustomView(charImg02)
         charListTabLayout.getTabAt(2)?.setCustomView(charImg03)
 
+        // 햄버거 아이콘 터치
+        naviBtn.setOnClickListener {
+            layout_drawer.openDrawer(GravityCompat.START) // START: left, END: right 와 같은 의미. 왼쪽에서부터 열림.
+        }
+
+        // 네비게이션 메뉴 터치 시
+        naviView.setNavigationItemSelectedListener(this)
+
+    }
+
+    //    네비게이션 메뉴의 아이템 선택 시 수행할 내용 지정
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.myInfo -> Toast.makeText(applicationContext, "내정보", Toast.LENGTH_SHORT).show()
+            R.id.friends -> Toast.makeText(applicationContext, "스케줄", Toast.LENGTH_SHORT).show()
+        }
+        layout_drawer.closeDrawers() // 선택이 끝났으니 네비게이션 메뉴 창은 닫아줘야한다.
+        return false
     }
 }
